6940b74fc738df2b6f447d44c63e07f6b820edc7
Eric Gustafson <ericg@elfwerks.io>
2022-11-21 22:15:49 -0700

    enhance: add extended key usages

--- pki.py
+++ pki.py
@@ -4,6 +4,7 @@ from cryptography import x509
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import ExtendedKeyUsageOID
 from cryptography.x509.oid import NameOID
 
 import click
@@ -55,7 +56,8 @@ def hostkeycert(ctx, hostnames):
         private_key.public_key(),
         sign_key,
         sign_cert,
-        hostnames
+        hostnames,
+        365, # days
     )
     write_key(private_key, KEY_PEM)
     write_cert(cert, CRT_PEM)
@@ -146,7 +148,7 @@ def create_key():
     )
 
 
-def new_certificate(pub_key, sign_key, sign_cert, hostnames):
+def new_certificate(pub_key, sign_key, sign_cert, hostnames, valid_days):
     one_day = datetime.timedelta(1, 0, 0)
     builder = x509.CertificateBuilder()
     builder = builder.subject_name(x509.Name([
@@ -162,19 +164,34 @@ def new_certificate(pub_key, sign_key, sign_cert, hostnames):
     #
     # TODO: parameterize lifetime of certificate
     #
-    builder = builder.not_valid_after(datetime.datetime.today() + (one_day * 30))
+    builder = builder.not_valid_after(datetime.datetime.today() +
+                                      (one_day * valid_days))
     builder = builder.serial_number(x509.random_serial_number())
     builder = builder.public_key(pub_key)
-    san = []
+    builder = builder.add_extension(
+        x509.KeyUsage(
+            digital_signature=True, content_commitment=False,
+            key_encipherment=True, data_encipherment=False,
+            key_agreement=False, key_cert_sign=False, crl_sign=False,
+            encipher_only=False, decipher_only=False), critical=True)
+    builder = builder.add_extension(
+        x509.ExtendedKeyUsage([
+            ExtendedKeyUsageOID.CLIENT_AUTH,
+            ExtendedKeyUsageOID.SERVER_AUTH]), critical=False)
+    builder = builder.add_extension(
+        x509.BasicConstraints(ca=False, path_length=None), critical=True)
+    builder = builder.add_extension(
+        x509.SubjectKeyIdentifier.from_public_key(pub_key), critical=False)
+    builder = builder.add_extension(
+        x509.AuthorityKeyIdentifier.from_issuer_public_key(sign_cert.public_key()),
+        critical=False)
+    san = []  # <- Subject Alternative Names
     for hn in hostnames:
         san.append(x509.DNSName(hn))
     builder = builder.add_extension(
         x509.SubjectAlternativeName(san), critical=False)
-    builder = builder.add_extension(
-        x509.BasicConstraints(ca=False, path_length=None), critical=True)
     return builder.sign(
-        private_key=sign_key, algorithm=hashes.SHA256(),
-    )
+        private_key=sign_key, algorithm=hashes.SHA256())
 
 
 def make_csr_builder():
